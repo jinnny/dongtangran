66b1433cc310bf4725d2c1be7689b3b50c660887
yang-eunjin <jinny920311@gmail.com>
2021-09-17 23:05:55 +0900

    pc 메인

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,11 @@
 $(function() {
 
   var mainSlide = function(){
-    var $slick_ele1 = $(".main_visual");
-    var $pager = $(".menu_con >li");
+    var $slick_ele1 = $('.main-visual-content');
+    var $pager = $('.main-visual-controller .visual-controller__item');
+    var $pagerProgressLine = $('.main-visual-controller .controller-progress-line');
 
-    $slick_ele1.on("init", function(slick){
+    $slick_ele1.on('init', function(slick){
       _bgMotion(0);
     });
 
@@ -21,109 +22,66 @@ $(function() {
     });
 
 
-    $pager.find(">a").on("click",function(e){
-      e.preventDefault();
+    $pager.on('click',function(){
       var slideno = $(this).data('slide');
       $slick_ele1.slick('slickGoTo', slideno);
     });
 
 
-    $slick_ele1.on("beforeChange", function(event, slick, currentSlide, nextSlide){
+    $slick_ele1.on('beforeChange', function(event, slick, currentSlide, nextSlide){
       _bgMotion(nextSlide);
-      var $current = $pager.eq(currentSlide);
-      $pager.addClass("on");
-      $current.removeClass("on");
+      $pagerProgressLine.css('height', ((nextSlide+1) * 33.33) + '%')
+      $pager.removeClass('on');
+      $pager.eq(nextSlide).addClass('on');
     });
 
     function _bgMotion(num){
-      var $nextLi = $slick_ele1.find(".slick-slide").eq(num);
-      TweenMax.set($(".obj_1"), {autoAlpha:0, y:80});
+      var $nextLi = $slick_ele1.find('.slick-slide').eq(num);
+      TweenMax.set($('.main-text-img'), {autoAlpha:0, y:80});
+      TweenMax.set($('.main-label-img'), {autoAlpha:0, y:80});
 
-      TweenMax.to($nextLi.find(".obj_1"), 1, {delay:.3, autoAlpha:1, y:0, ease:Power2.easeOut});
-    };
-
+      TweenMax.to($nextLi.find('.main-text-img'), 1, {delay:.5, autoAlpha:1, y:0, ease:Power2.easeOut});
+      TweenMax.to($nextLi.find('.main-label-img'), 1, {delay:.3, autoAlpha:1, y:0, ease:Power2.easeOut});
+    }
   }
-  // 메인슬라이드
-  var  mainVisual = new Swiper('.main-visual', {
-    paginationClickable: true,
-    pagination: {
-      el: '.swiper-pagination', clickable: !1,
-      renderBullet: function (index, className) {
-        return '<span class="pagination__item ' + className +'">'+0 + (index + 1) + '<span class="pagination-bar"></span>' + "</span>";
-      },
-    },
-    autoplay: { delay: 5e3, disableOnInteraction: !1 },
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    effect: 'fade',
-    on: {
-      init: function () {
+
+  var premiumSlide = function() {
+    var $premium_ele = $('.main-premium-slide');
+    var $pagerProgressLine = $('.main-premium .controller-progress-line');
+    var $pagerNumber1 = $('.main-premium .controller__number1');
+    var $pagerNumber3 = $('.main-premium .controller__number3');
+    $premium_ele.slick({
+      infinite: true,
+      autoplay: true,
+      arrows: true,
+      autoplaySpeed: 2500,
+      speed: 2500,
+      dots: false,
+      focusOnSelect: false,
+      draggable: false
+    });
+
+    $premium_ele.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      var active = nextSlide+1
+      $pagerProgressLine.css('width', (active * 33.33) + '%')
+    });
+
+    $('.js-autoplay-control-button').on('click', function () {
+      if($(this).hasClass('autoplay-stop-icon')) {
+        $premium_ele.slick('slickPause');
+        $(this).addClass('is--hide')
+        $('.autoplay-play-icon').removeClass('is--hide')
+      }else {
+        $premium_ele.slick('slickPlay');
+        $(this).addClass('is--hide')
+        $('.autoplay-stop-icon').removeClass('is--hide')
       }
-    }
-  });
-  $('.swiper-autoplay-control-button').on('click', function () {
-    if($(this).hasClass('autoplay-stop-icon')) {
-      mainVisual.autoplay.stop()
-      $(this).addClass('is--hide')
-      $('.autoplay-play-icon').removeClass('is--hide')
-    }else {
-      mainVisual.autoplay.start()
-      $(this).addClass('is--hide')
-      $('.autoplay-stop-icon').removeClass('is--hide')
-    }
-  })
+    })
+  }
 
 
-  new Swiper('.community-swiper-container', {
-    pagination: { el: '.swiper-pagination.progress', clickable: true },
-    // autoplay: { delay: 5e3, disableOnInteraction: !1 },
-    autoplay: {
-      delay: 5000
-    },
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-    on: {
-      init: function () {
-        TweenMax.to($('.swiper-slide-active .community-slide__title span'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide__title span'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-line'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-line'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.3});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text--img img'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text--img img'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.6});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text--impact p'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text--impact p'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.9});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text:not(.community-slide-text--img) p'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text:not(.community-slide-text--img) p'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 1.2});
-          }})
-        TweenMax.to($('.swiper-slide-active .community__link a'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community__link a'), 1, {y: 50,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 1.5});
-          }})
-      },
-      slideChange : function(){
-        TweenMax.to($('.swiper-slide-active .community-slide__title span'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide__title span'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-line'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-line'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.3});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text--img img'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text--img img'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.6});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text--impact p'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text--impact p'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 0.9});
-          }})
-        TweenMax.to($('.swiper-slide-active .community-slide-text:not(.community-slide-text--img) p'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community-slide-text:not(.community-slide-text--img) p'), 1, {y: 80,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 1.2});
-          }})
-        TweenMax.to($('.swiper-slide-active .community__link a'),0.35,{y:0,autoAlpha : 0,onComplete :function(){
-            TweenMax.fromTo($('.swiper-slide-active .community__link  a'), 1, {y: 50,autoAlpha:0,ease: Power3.easeOut,delay: 0}, { y: 0,autoAlpha:1,delay: 1.5});
-          }})
-      }
-    }
-  });
-
+  mainSlide()
+  premiumSlide()
 
   $('.js-main-quick-button').on('click', function () {
     $(this).toggleClass('is--active')
